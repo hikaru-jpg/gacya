2b90e7ae75570878179bacc4896b44ced0175ff4
hikaru-jpg <hikaru20050413@gmail.com>
2025-03-02 01:57:50 +0900

    Update compile.cpp

--- gacya/compile.cpp
+++ gacya/compile.cpp
@@ -9,10 +9,9 @@ using namespace std;
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
     long long gacya(long long value) {
-        random_device rd;
-        seed_seq seed{rd()};  
+        long long seed_val = std::chrono::steady_clock::now().time_since_epoch().count();
         mt19937 gen(seed); 
-        uniform_int_distribution<> dis(1, value);
+        uniform_int_distribution<long long> dis(1, value);
     
             long long random_number = dis(gen);
             return random_number;
