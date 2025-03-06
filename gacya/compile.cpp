905586964154efe7e79d510255487f86aefb31b2
hikaru-jpg <hikaru20050413@gmail.com>
2025-03-07 02:18:04 +0900

    Update compile.cpp

--- gacya/compile.cpp
+++ gacya/compile.cpp
@@ -4,16 +4,18 @@
 #include <random>
 #include <emscripten.h>
 #include <vector>
+#include <chrono> 
 using namespace std;
 
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
     int gacya(int value) {
-        long long seed_val = std::chrono::steady_clock::now().time_since_epoch().count();
-        mt19937 gen(seed); 
-        uniform_int_distribution<long long> dis(1, value);
+        long long seed = std::chrono::steady_clock::now().time_since_epoch().count();
+        std::seed_seq seq{static_cast<uint32_t>(seed & 0xFFFFFFFF), static_cast<uint32_t>(seed >> 32)};
+        mt19937 gen(seq);
+        uniform_int_distribution<int> dis(1, value);
     
-            long long random_number = dis(gen);
+            int random_number = dis(gen);
             return random_number;
         
     }
