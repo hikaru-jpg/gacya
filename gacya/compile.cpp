e11ad062abab834e53c7abc7309f179e778bd7db
hikaru-jpg <hikaru20050413@gmail.com>
2025-03-08 15:24:51 +0900

    Update compile.cpp

--- gacya/compile.cpp
+++ gacya/compile.cpp
@@ -11,7 +11,7 @@ extern "C" {
     EMSCRIPTEN_KEEPALIVE
     int gacya(int value) {
         long long seed = std::chrono::steady_clock::now().time_since_epoch().count();
-        std::seed_seq seq{static_cast<uint32_t>(seed & 0xFFFFFFFF), static_cast<uint32_t>(seed >> 32)};
+        seed_seq seq{static_cast<uint32_t>(seed & 0xFFFFFFFF), static_cast<uint32_t>(seed >> 32)};
         mt19937 gen(seq);
         uniform_int_distribution<int> dis(1, value);
     
