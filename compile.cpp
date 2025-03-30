d5250b143cfaff1f802993ec755574ba8197ef5a
hikaru-jpg <hikaru20050413@gmail.com>
2025-03-30 22:32:10 +0900

    aaaaaaaa

--- compile.cpp
+++ compile.cpp
@@ -19,12 +19,12 @@ extern "C" {
             return (random_number == 1) ? 1 : 0;
     }
 
+    EMSCRIPTEN_KEEPALIVE
     int gacyas(int value) {
         long long seed = chrono::steady_clock::now().time_since_epoch().count();
         seed_seq seq{static_cast<uint32_t>(seed & 0xFFFFFFFF), static_cast<uint32_t>(seed >> 32)};
         mt19937 gen(seq);
         uniform_int_distribution<int> dis(1, value);
-    
             int random_number = dis(gen);
             return (random_number == 1) ? 1 : 0;
     }
