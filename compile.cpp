ecb7244ad7d00c64110dbb778584f580ee29d991
hikaru-jpg <hikaru20050413@gmail.com>
2025-04-02 00:32:59 +0900

    aaaaaaaaaaaaadd

--- compile.cpp
+++ compile.cpp
@@ -20,12 +20,19 @@ extern "C" {
     }
 
     EMSCRIPTEN_KEEPALIVE
-    int gacyas(int value) {
+    int* gacyas(int value) {
+        static int results[10]; 
+
         long long seed = chrono::steady_clock::now().time_since_epoch().count();
         seed_seq seq{static_cast<uint32_t>(seed & 0xFFFFFFFF), static_cast<uint32_t>(seed >> 32)};
         mt19937 gen(seq);
         uniform_int_distribution<int> dis(1, value);
+
+        for(int i = 0;i<10;i++){
             int random_number = dis(gen);
-            return (random_number == 1) ? 1 : 0;
+            results[i] = (random_number == 1) ? 1 : 0;
+        }
+            
+        return results;
     }
 }
